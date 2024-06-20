8b28eb67e5d74584f3bf0a5de93ec4da9fedaffe
Valmire Nivokazi <Valmire.nivokazi@novatec-gmbh.de>
2024-06-20 16:08:54 +0200

    neue Änderungen

--- convert.js
+++ convert.js
@@ -4,18 +4,55 @@ const path = require('path');
 const inputFilePath = path.resolve(__dirname, 'tokens.json');
 const outputFilePath = path.resolve(__dirname, 'dist', 'tokens.css');
 
+// Funktion zum Auflösen von Token-Referenzen
+function resolveReferences(obj, map = new Map()) {
+  for (const key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      resolveReferences(obj[key], map);
+    } else if (key === 'value') {
+      map.set(obj[key], key);
+    }
+  }
+  for (const key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      for (const k in obj[key]) {
+        if (typeof obj[key][k] === 'string' && map.has(obj[key][k])) {
+          obj[key][k] = obj[key][k].replace(/{(.*?)}/g, (_, v) => obj[v]?.value || v);
+        }
+      }
+    }
+  }
+  return obj;
+}
+
 fs.readFile(inputFilePath, 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading JSON file:', err);
     process.exit(1);
   }
 
-  const tokens = JSON.parse(data);
-  let cssContent = '';
-
-  for (const [key, value] of Object.entries(tokens)) {
-    cssContent += `--${key}: ${value};\n`;
+  let tokens;
+  try {
+    tokens = JSON.parse(data);
+  } catch (jsonErr) {
+    console.error('Error parsing JSON:', jsonErr);
+    process.exit(1);
   }
+
+  tokens = resolveReferences(tokens);
+
+  let cssContent = '';
+  function processTokens(obj, prefix = '') {
+    for (const key in obj) {
+      if (typeof obj[key] === 'object' && obj[key] !== null && obj[key].value) {
+        cssContent += `--${prefix}${key}: ${obj[key].value};\n`;
+      } else if (typeof obj[key] === 'object' && obj[key] !== null) {
+        processTokens(obj[key], `${prefix}${key}-`);
+      }
+    }
+  }
+
+  processTokens(tokens);
 
   fs.mkdirSync(path.dirname(outputFilePath), { recursive: true });
   fs.writeFile(outputFilePath, cssContent, 'utf8', (err) => {
