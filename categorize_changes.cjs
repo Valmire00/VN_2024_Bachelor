8ef11f94dcb2efaee22432f39e72fa8867a29c47
Valmire Nivokazi <Valmire.nivokazi@novatec-gmbh.de>
2024-07-01 23:20:30 +0200

    Kategorisierung angepasst

--- categorize_changes.cjs
+++ categorize_changes.cjs
@@ -36,53 +36,76 @@ const report = [
 fs.writeFileSync(outputFilePath, report);
 */
 
-const fs = require('fs');
+import fs from 'fs';
 
-// Lesen der CSS-Dateien
-const oldCSS = fs.readFileSync(process.argv[3], 'utf8');
-const newCSS = fs.readFileSync(process.argv[2], 'utf8');
-
-// Parsing der CSS-Dateien in Zeilen
-const oldLines = oldCSS.split('\n');
-const newLines = newCSS.split('\n');
-
-// Ändern Sie die Kategorien in "simple" und "fatal"
-const simpleChanges = [];
-const fatalChanges = [];
-
-// Token-Tracking für das Erkennen von doppelten Token
-const tokenSet = new Set();
-
-// Überprüfen von Änderungen zwischen alten und neuen Zeilen
-const maxLength = Math.max(oldLines.length, newLines.length);
-for (let i = 0; i < maxLength; i++) {
-    const oldLine = oldLines[i] || '';
-    const newLine = newLines[i] || '';
-
-    // Überprüfen, ob eine Zeile hinzugefügt oder entfernt wurde
-    if (!oldLine && newLine) {
-        // Überprüfen auf doppelte Token
-        const tokenMatch = newLine.match(/(--[\w-]+):/);
-        if (tokenMatch && tokenSet.has(tokenMatch[1])) {
-            fatalChanges.push(`Duplicate token: ${tokenMatch[1]} added in new file at line ${i + 1}`);
-        } else {
-            tokenSet.add(tokenMatch ? tokenMatch[1] : '');
-            simpleChanges.push(`Added: ${newLine} at line ${i + 1}`);
-        }
-    } else if (oldLine && !newLine) {
-        fatalChanges.push(`Removed: ${oldLine} at line ${i + 1}`);
-    } else if (oldLine !== newLine) {
-        simpleChanges.push(`Changed from "${oldLine}" to "${newLine}" at line ${i + 1}`);
-    }
+// Hilfsfunktion zum Einlesen von CSS-Dateien
+function readCSSFile(filePath) {
+    return fs.readFileSync(filePath, 'utf8');
 }
 
-// Schreiben der Ergebnisse in die Datei
-const result = [
-    'Simple Changes:',
-    ...simpleChanges,
-    '',
-    'Fatal Changes:',
-    ...fatalChanges
-].join('\n');
+// Funktion zur Extraktion der Tokens aus der CSS-Datei
+function extractTokens(cssContent) {
+    const tokenRegex = /(--[^:]+):\s*([^;]+);/g;
+    const tokens = {};
+    let match;
+    while ((match = tokenRegex.exec(cssContent)) !== null) {
+        tokens[match[1]] = match[2];
+    }
+    return tokens;
+}
 
-fs.writeFileSync(process.argv[4], result, 'utf8');
+// Funktion zur Kategorisierung der Änderungen
+function categorizeChanges(newTokens, oldTokens) {
+    const simpleChanges = [];
+    const criticalChanges = [];
+
+    const allKeys = new Set([...Object.keys(newTokens), ...Object.keys(oldTokens)]);
+
+    allKeys.forEach(key => {
+        if (!(key in oldTokens)) {
+            criticalChanges.push(`New token added: ${key}: ${newTokens[key]}`);
+        } else if (!(key in newTokens)) {
+            criticalChanges.push(`Token removed: ${key}`);
+        } else if (newTokens[key] !== oldTokens[key]) {
+            simpleChanges.push(`Changed ${key} from "${oldTokens[key]}" to "${newTokens[key]}"`);
+        }
+    });
+
+    // Doppelte Tokens suchen und als kritische Änderung kennzeichnen
+    const tokenMap = new Map();
+    for (const key in newTokens) {
+        if (tokenMap.has(key)) {
+            criticalChanges.push(`Duplicate token detected: ${key}`);
+        } else {
+            tokenMap.set(key, newTokens[key]);
+        }
+    }
+
+    return { simpleChanges, criticalChanges };
+}
+
+// Hauptfunktion
+function main() {
+    const [newFilePath, oldFilePath, outputFilePath] = process.argv.slice(2);
+
+    const newCSSContent = readCSSFile(newFilePath);
+    const oldCSSContent = readCSSFile(oldFilePath);
+
+    const newTokens = extractTokens(newCSSContent);
+    const oldTokens = extractTokens(oldCSSContent);
+
+    const { simpleChanges, criticalChanges } = categorizeChanges(newTokens, oldTokens);
+
+    const categorizedChanges = [
+        'Simple Changes:',
+        ...simpleChanges,
+        '',
+        'Critical Changes:',
+        ...criticalChanges
+    ].join('\n');
+
+    fs.writeFileSync(outputFilePath, categorizedChanges);
+    console.log('Changes categorized and written to', outputFilePath);
+}
+
+main();
