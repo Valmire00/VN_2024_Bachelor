5f8a7395e41c5f01d9f343cfa14f185b3f549d06
Valmire Nivokazi <Valmire.nivokazi@novatec-gmbh.de>
2024-07-03 15:54:45 +0200

    Änderungen an der Kategorisierungen

--- categorize_json_changes.cjs
+++ categorize_json_changes.cjs
@@ -143,9 +143,9 @@ function categorizeChanges(newTokensDir, oldTokensDir) {
   // Compare tokens
   for (const [key, value] of Object.entries(newTokens)) {
     if (!(key in oldTokens)) {
-      criticalChanges.push(`Added: ${key} with value ${value}`);
-    } else if (oldTokens[key] !== value) {
-      simpleChanges.push(`Modified: ${key} from ${oldTokens[key]} to ${value}`);
+      criticalChanges.push(`Added: ${key} with value ${JSON.stringify(value)}`);
+    } else if (JSON.stringify(oldTokens[key]) !== JSON.stringify(value)) {
+      simpleChanges.push(`Modified: ${key} from ${JSON.stringify(oldTokens[key])} to ${JSON.stringify(value)}`);
     }
   }
 
@@ -153,7 +153,7 @@ function categorizeChanges(newTokensDir, oldTokensDir) {
     if (!(key in newTokens)) {
       criticalChanges.push(`Removed: ${key}`);
     }
-  };
+  }
 
   return { simpleChanges, criticalChanges };
 }
