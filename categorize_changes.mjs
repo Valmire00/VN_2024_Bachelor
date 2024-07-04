ffedf48c199f120bd04d1d21e908d1912ca4c74e
Valmire Nivokazi <Valmire.nivokazi@novatec-gmbh.de>
2024-07-04 23:26:22 +0200

    changes

--- categorize_changes.mjs
+++ categorize_changes.mjs
@@ -237,7 +237,6 @@ main();
 */
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
